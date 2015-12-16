f202051a3fae2060bf96ee8abeaaaab7363a88c9
Shroomy Shroom <sporeventexplosion@gmail.com>
2015-12-16 17:54:20 +0800

    Change trailer_youtube_url to trailer_youtube_id
    Avoids unnecessary regex logic that converts trailer_youtube_url to
    a YouTube video id code in fresh_tomatoes.py

--- media.py
+++ media.py
@@ -5,14 +5,14 @@ from fresh_tomatoes import open_movies_page
 MOVIES_FILENAME = 'movies.json'
 
 class Movie():
-    def __init__(self, title, description, poster_image_url, trailer_youtube_url):
+    def __init__(self, title, description, poster_image_url, trailer_youtube_id):
         self.title = title
         self.description = description
         self.poster_image_url = poster_image_url
-        self.trailer_youtube_url = trailer_youtube_url
+        self.trailer_youtube_id = trailer_youtube_id
 
 def movie_from_dict(movie_dict):
-    return Movie(movie_dict['name'], movie_dict['description'], movie_dict['poster_image_url'], movie_dict['trailer_youtube_url'])
+    return Movie(movie_dict['name'], movie_dict['description'], movie_dict['poster_image_url'], movie_dict['trailer_youtube_id'])
 
 def open_from_json(filename):
     movie_file = open(filename)
