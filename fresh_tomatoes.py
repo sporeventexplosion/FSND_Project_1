d4e978d4f1fc3484ba238bc8e63a0dc2748def2e
Shroomy Shroom <sporeventexplosion@gmail.com>
2015-12-18 16:53:02 +0800

    Stylistic changes to comply with PEP8

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -2,6 +2,7 @@ import webbrowser
 import os
 import cgi
 
+
 def get_file_contents(filename):
     file_obj = open(filename)
     text = file_obj.read()
@@ -11,22 +12,25 @@ def get_file_contents(filename):
 page_main = get_file_contents('./template/main.html')
 movie_tile = get_file_contents('./template/tile.html')
 
+
 def create_movie_tiles_content(movies):
     # The HTML content for this section of the page
     content = ''
     for movie in movies:
         content += render_template(movie_tile,
-            movie_title=movie.title,
-            movie_description=movie.description,
-            poster_image_url=movie.poster_image_url,
-            trailer_youtube_id=movie.trailer_youtube_id
-        )
+                                   movie_title=movie.title,
+                                   movie_description=movie.description,
+                                   poster_image_url=movie.poster_image_url,
+                                   trailer_youtube_id=movie.trailer_youtube_id)
     return content
 
 # A very simple function for rendering a template string.
-# Made to use double curly braces to not get 'tripped up' by CSS like .format does
+# Made to use double curly braces to not get 'tripped up' by CSS like .format
 # Escape is optional so values are not double-escaped
+
+
 def render_template(template, escape=True, **kwargs):
+
     for item in kwargs.items():
         # sanitize the values passed in
         if escape:
@@ -38,14 +42,17 @@ def render_template(template, escape=True, **kwargs):
 
     return template
 
+
 def open_movies_page(movies):
     # Create or overwrite the output file
     output_file = open('fresh_tomatoes.html', 'w')
 
+    rendered_tiles = create_movie_tiles_content(movies)
+
     # Replace the movie tiles placeholder generated content
     # Avoid escaping the already-escaped content from create_movie_tile_content
     rendered_content = render_template(page_main, escape=False,
-        movie_tiles=create_movie_tiles_content(movies))
+                                       movie_tiles=rendered_tiles)
 
     # Output the file
     output_file.write(rendered_content)
